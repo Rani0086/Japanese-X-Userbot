f38f59cee0b28d24c5f9840a6532a2b053a73fc6
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-22 21:49:38 +0530

    Update __init__.py done

--- X/__init__.py
+++ X/__init__.py
@@ -1,25 +1,3 @@
-#MIT License
-
-#Copyright (c) 2024 Japanese-X-Userbot
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
 import asyncio
 import logging
 import sys
@@ -40,7 +18,6 @@ from config import (
     API_HASH,
     API_ID,
     DB_URL,
-    BOTLOG_CHATID,
     STRING_SESSION1,
     STRING_SESSION2,
     STRING_SESSION3,
@@ -51,7 +28,6 @@ from config import (
     STRING_SESSION8,
     STRING_SESSION9,
     STRING_SESSION10,
-    SUDO_USERS,
     BOT_TOKEN
 )
 DATABASE_URL = DB_URL
@@ -106,11 +82,6 @@ if not BOT_TOKEN:
    LOGGER(__name__).warning("WARNING: BOT TOKEN NOT FOUND, SHUTDOWN BOT")
    sys.exit
 
-
-if BOTLOG_CHATID:
-   BOTLOG_CHATID = BOTLOG_CHATID
-else:
-   BOTLOG_CHATID = "me"
 
 LOOP = asyncio.get_event_loop()
 
