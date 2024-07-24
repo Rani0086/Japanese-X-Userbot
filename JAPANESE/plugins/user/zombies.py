7958440d3f6980b9ec656495e8e18f7004206e22
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-24 19:21:50 +0530

    Create zombies.py

---
+++ JAPANESE/plugins/user/zombies.py
@@ -0,0 +1,10 @@
+import html
+
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+
+from config import CMD_HANDLER
+from JAPANESE.nxtgenhelper.basic import edit_or_reply
+from JAPANESE.nxtgenhelper.parser import mention_html, mention_markdown
+from .help import *
+
