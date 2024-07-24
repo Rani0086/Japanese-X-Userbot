91c64844d2dde4924e848506fefdd781cdf79e20
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-24 21:49:22 +0530

    Update tagall.py remake 5.0

--- JAPANESE/plugins/user/tagall.py
+++ JAPANESE/plugins/user/tagall.py
@@ -11,15 +11,23 @@ from .help import *
 @Client.on_message(filters.command(["tagall"], cmd) & filters.me)
 
 async def tag_all_users(client: Client, message: Message):
+    # Delete the original message
     await message.delete()
+
+    # Extract the text to be added to mentions
     if len(message.text.split()) >= 2:
         text = message.text.split(None, 1)[1]
     else:
         text = "𝐇𝐞𝐥𝐥𝐨 𝐉𝐀𝐏𝐀𝐍𝐄𝐒𝐄 𝐒𝐀𝐌𝐔𝐑𝐀𝐈'𝐒 😊"
-    nobi = client.get_chat_members(message.chat.id)
-    async for a in nobi:
-        if not a.user.is_bot:
-            text += mention_html(a.user.id, "\u200b")
+
+    # Get all non-bot members in the chat
+    chat_members = [m.user for m in await client.get_chat_members(message.chat.id) if not m.user.is_bot]
+
+    # Append mentions to the text
+    for user in chat_members:
+        text += mention_html(user.id, "\u200b")
+
+    # Send the updated text as a message
     if message.reply_to_message:
         await client.send_message(
             message.chat.id,
@@ -31,3 +39,4 @@ async def tag_all_users(client: Client, message: Message):
         await client.send_message(
             message.chat.id, text, parse_mode=enums.ParseMode.HTML
         )
+        
