1b2ceeacadd476e8fcd3487b248b567d927d5f94
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-24 21:37:54 +0530

    Update report.py

--- JAPANESE/plugins/user/report.py
+++ JAPANESE/plugins/user/report.py
@@ -10,3 +10,47 @@ from .help import *
 
 @Client.on_message(filters.command(["report"], cmd) & filters.me)
 
+async def report_admin(client: Client, message: Message):
+    await message.delete()
+    if len(message.text.split()) >= 2:
+        text = message.text.split(None, 1)[1]
+    else:
+        text = None
+    grup = await client.get_chat(message.chat.id)
+    admin = []
+    async for a in client.get_chat_members(
+        message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+    ):
+        if (
+            a.status == enums.ChatMemberStatus.ADMINISTRATOR
+            or a.status == enums.ChatMemberStatus.OWNER
+        ):
+            if not a.user.is_bot:
+                admin.append(mention_html(a.user.id, "\u200b"))
+    if message.reply_to_message:
+        if text:
+            sakura = "{}".format(sakura)
+        else:
+            sakura = "{} reported to admins.".format(
+                mention_html(
+                    message.reply_to_message.from_user.id,
+                    message.reply_to_message.from_user.first_name,
+                )
+            )
+    else:
+        if text:
+            sakura = "{}".format(html.escape(text))
+        else:
+            sakura = "𝐂𝐚𝐥𝐥𝐢𝐧𝐠 𝐚𝐝𝐦𝐢𝐧𝐬 𝐢𝐧 {}.".format(grup.title)
+    sakura += "".join(admin)
+    if message.reply_to_message:
+        await client.send_message(
+            message.chat.id,
+            sakura,
+            reply_to_message_id=message.reply_to_message.id,
+            parse_mode=enums.ParseMode.HTML,
+        )
+    else:
+        await client.send_message(
+            message.chat.id, sakura, parse_mode=enums.ParseMode.HTML
+  )
