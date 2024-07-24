9ff41b045e4d31c7eaba4eabd6ebc12cdf860a07
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-24 21:44:08 +0530

    Update tagall.py

--- JAPANESE/plugins/user/tagall.py
+++ JAPANESE/plugins/user/tagall.py
@@ -10,3 +10,24 @@ from .help import *
 
 @Client.on_message(filters.command(["tagall"], cmd) & filters.me)
 
+async def tag_all_users(client: Client, message: Message):
+    await message.delete()
+    if len(message.text.split()) >= 2:
+        text = message.text.split(None, 1)[1]
+    else:
+        text = "𝐇𝐞𝐥𝐥𝐨 𝐉𝐀𝐏𝐀𝐍𝐄𝐒𝐄 𝐒𝐀𝐌𝐔𝐑𝐀𝐈'𝐒 😊"
+    nobi = client.get_chat_members(message.chat.id)
+    async for a in nobi:
+        if not a.user.is_bot:
+            text += mention_html(a.user.id, "\u200b")
+    if message.reply_to_message:
+        await client.send_message(
+            message.chat.id,
+            text,
+            reply_to_message_id=message.reply_to_message.id,
+            parse_mode=enums.ParseMode.HTML,
+        )
+    else:
+        await client.send_message(
+            message.chat.id, text, parse_mode=enums.ParseMode.HTML
+        )
