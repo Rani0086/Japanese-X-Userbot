15d8da85c3c83592c9ca013bdd8e4a1851526bd9
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-24 20:55:16 +0530

    Update tagall.py

--- JAPANESE/plugins/user/tagall.py
+++ JAPANESE/plugins/user/tagall.py
@@ -1,1 +1,12 @@
+import html
+
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+
+from config import CMD_HANDLER
+from JAPANESE.nxtgenhelper.basic import edit_or_reply
+from JAPANESE.nxtgenhelper.parser import mention_html, mention_markdown
+from .help import *
+
+@Client.on_message(filters.command(["tagall"], cmd) & filters.me)
 
