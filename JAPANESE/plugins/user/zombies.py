9a38b6ea7de3f23a16df3ae25f4791b150f8485f
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-24 19:23:18 +0530

    Update zombies.py

--- JAPANESE/plugins/user/zombies.py
+++ JAPANESE/plugins/user/zombies.py
@@ -8,3 +8,14 @@ from JAPANESE.nxtgenhelper.basic import edit_or_reply
 from JAPANESE.nxtgenhelper.parser import mention_html, mention_markdown
 from .help import *
 
+@Client.on_message(filters.command(["zombies"], cmd) & filters.me)
+
+async def kickdel_cmd(client: Client, message: Message):
+    nobi = await edit_or_reply(message, "<b>𝐊𝐢𝐜𝐤𝐢𝐧𝐠 𝐝𝐞𝐥𝐞𝐭𝐞𝐝 𝐚𝐜𝐜𝐨𝐮𝐧𝐭𝐬...</b>")
+    # noinspection PyTypeChecker
+    values = [
+        await message.chat.ban_member(user.user.id, int(time()) + 31)
+        for member in await message.chat.get_members()
+        if member.user.is_deleted
+    ]
+    await nobi.edit(f"<b>𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐤𝐢𝐜𝐤𝐞𝐝 {len(values)} 𝐝𝐞𝐥𝐞𝐭𝐞𝐝 𝐚𝐜𝐜𝐨𝐮𝐧𝐭(s)</b>")
