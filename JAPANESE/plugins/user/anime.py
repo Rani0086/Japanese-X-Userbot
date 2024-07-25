a74f0cdd268aa651ce57a3702aa51e733d6ee57b
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 16:30:27 +0530

    Update anime.py

--- JAPANESE/plugins/user/anime.py
+++ JAPANESE/plugins/user/anime.py
@@ -1,1 +1,10 @@
+import aiohttp
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from config import CMD_HANDLER
+from JAPANESE.nxtgenhelper.PyroHelpers import GetChatID, ReplyCheck
+from .help import * 
 
+
+
+@Client.on_message(filters.command(["genshin"], cmd) & filters.me)
