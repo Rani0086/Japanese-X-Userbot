d2666ecfc7cbef82c9b454d4fd1d9b05816db35a
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 16:00:20 +0530

    Update demote.py

--- JAPANESE/plugins/user/demote.py
+++ JAPANESE/plugins/user/demote.py
@@ -21,3 +21,26 @@ unmute_permissions = ChatPermissions(
 )
 
 @Client.on_message(filters.command(["demote"], cmd) & filters.me)
+
+async def demote(client: Client, message: Message):
+    user_id = await extract_user(message)
+    X = await edit_or_reply(message, "`Processing...`")
+    if not user_id:
+        return await X.edit("I can't find that user.")
+    if user_id == client.me.id:
+        return await X.edit("I can't demote myself.")
+    await message.chat.promote_member(
+        user_id,
+        privileges=ChatPrivileges(
+            can_manage_chat=False,
+            can_delete_messages=False,
+            can_manage_video_chats=False,
+            can_restrict_members=False,
+            can_change_info=False,
+            can_invite_users=False,
+            can_pin_messages=False,
+            can_promote_members=False,
+        ),
+    )
+    umention = (await client.get_users(user_id)).mention
+    await X.edit(f"Demoted! {umention}")
