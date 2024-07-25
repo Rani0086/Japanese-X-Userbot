cb02afc2356dd752258adeaa0063413e17c8967d
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 16:45:08 +0530

    Update anime.py remake 5.0.......√

--- JAPANESE/plugins/user/anime.py
+++ JAPANESE/plugins/user/anime.py
@@ -259,3 +259,48 @@ async def give_bully(bot: Client, message: Message):
                 ),
             )
 
+@Client.on_message(filters.command(["sunglasses"], cmd) & filters.me)
+async def give_bully(bot: Client, message: Message):
+    URL = "https://fantox-apis.vercel.app/sunglasses"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL) as request:
+            if request.status == 404:
+                return await message.edit("`no anime for you :c")
+            result = await request.json()
+            url = result.get("url", None)
+            
+            if message.reply_to_message and message.reply_to_message.from_user:
+                replied_user = message.reply_to_message.from_user
+                user_first_name = replied_user.first_name
+                user_id = replied_user.id
+                user_link = f"[{user_first_name}](tg://user?id={user_id})"
+                caption = f"ғᴏʀ ʏᴏᴜ {user_link}"
+            else:
+                return await message.edit("ᴘʟᴇᴀꜱᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇꜱꜱᴀɢᴇ ᴛᴏ ʀᴇᴀᴄᴛ ᴛᴏ ꜱᴏᴍᴇᴏɴᴇ.")
+            
+            await asyncio.gather(
+                message.delete(),
+                bot.send_video(
+                    GetChatID(message), url, 
+                    reply_to_message_id=ReplyCheck(message),
+                    caption=caption
+                ),
+            )
+
+
+add_command_help(
+    "•─╼⃝𖠁 ᴀɴɪᴍᴇ",
+    [
+       ["genshin", "Gɪᴠᴇ genshin."],
+       ["swimsuit", "Gɪᴠᴇ swimsuit ᴀɴɪᴍᴀᴛɪᴏɴ."],
+       ["schoolswimsuit", "Gɪᴠᴇ schoolswimsuit ᴀɴɪᴍᴀᴛɪᴏɴ."],
+       ["white", "Gɪᴠᴇ white ᴀɴɪᴍᴀᴛɪᴏɴ."],
+       ["barefoot", "Gɪᴠᴇ barefoot ᴀɴɪᴍᴀᴛɪᴏɴ."],
+       ["touhou", "Give touhou animation."],
+       ["gamecg", "Give gamecg animation."],
+       ["hololive", "Give hololive animation."],
+       ["uncensored", "Give uncensored animation."],
+       ["sunglasses", "Give sunglasses animation."],
+        ],
+)
+
