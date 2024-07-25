e06d3ba960e6aad4115d36d6eb1e68641ecb1c37
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 15:54:41 +0530

    Update promote.py

--- JAPANESE/plugins/user/promote.py
+++ JAPANESE/plugins/user/promote.py
@@ -21,3 +21,43 @@ unmute_permissions = ChatPermissions(
 )
 
 @Client.on_message(filters.command(["promote", "fullpromote"], cmd) & filters.me)
+
+async def promotte(client: Client, message: Message):
+    user_id = await extract_user(message)
+    umention = (await client.get_users(user_id)).mention
+    X = await edit_or_reply(message, "`Processing...`")
+    if not user_id:
+        return await X.edit("I can't find that user.")
+    bot = (await client.get_chat_member(message.chat.id, client.me.id)).privileges
+    if not bot.can_promote_members:
+        return await X.edit("I don't have enough permissions")
+    if message.command[0][0] == "f":
+        await message.chat.promote_member(
+            user_id,
+            privileges=ChatPrivileges(
+                can_manage_chat=True,
+                can_delete_messages=True,
+                can_manage_video_chats=True,
+                can_restrict_members=True,
+                can_change_info=True,
+                can_invite_users=True,
+                can_pin_messages=True,
+                can_promote_members=True,
+            ),
+        )
+        return await X.edit(f"Fully Promoted! {umention}")
+
+    await message.chat.promote_member(
+        user_id,
+        privileges=ChatPrivileges(
+            can_manage_chat=True,
+            can_delete_messages=True,
+            can_manage_video_chats=True,
+            can_restrict_members=True,
+            can_change_info=True,
+            can_invite_users=True,
+            can_pin_messages=True,
+            can_promote_members=False,
+        ),
+    )
+    await X.edit(f"Promoted! {umention}")
