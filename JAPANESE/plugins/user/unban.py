3cf3e9fbcb67c1ebf257153334ae025855838bdb
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 12:41:49 +0530

    Update unban.py

--- JAPANESE/plugins/user/unban.py
+++ JAPANESE/plugins/user/unban.py
@@ -22,3 +22,23 @@ unmute_permissions = ChatPermissions(
 
 @Client.on_message(filters.command(["unban"], cmd) & filters.me)
 
+async def member_unban(client: Client, message: Message):
+    reply = message.reply_to_message
+    X = await edit_or_reply(message, "`In progresss master...`")
+    bot = (await client.get_chat_member(message.chat.id, client.me.id)).privileges
+    if not bot.can_restrict_members:
+        return await X.edit("Ask Admin First dude!")
+    if reply and reply.sender_chat and reply.sender_chat != message.chat.id:
+        return await X.edit("It's a channel, where can you ban it, okay?!")
+
+    if len(message.command) == 2:
+        user = message.text.split(None, 1)[1]
+    elif len(message.command) == 1 and reply:
+        user = message.reply_to_message.from_user.id
+    else:
+        return await X.edit(
+            "Username where is the fool?!."
+        )
+    await message.chat.unban_member(user)
+    umention = (await client.get_users(user)).mention
+    await X.edit(f"Unbanned! {umention}")
