d827587ab6380afbd288dd518edc1400b1bed9e2
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 12:33:21 +0530

    Create ban.py

---
+++ JAPANESE/plugins/user/ban.py
@@ -0,0 +1,55 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.errors import ChatAdminRequired
+from pyrogram.types import ChatPermissions, ChatPrivileges, Message
+
+from config import CMD_HANDLER
+from JAPANESE.nxtgenhelper.adminHelpers import DEVS
+from JAPANESE.nxtgenhelper.basic import edit_or_reply
+from .help import *
+from JAPANESE.utils.misc import extract_user, extract_user_and_reason, list_admins
+
+unmute_permissions = ChatPermissions(
+    can_send_messages=True,
+    can_send_media_messages=True,
+    can_send_polls=True,
+    can_change_info=False,
+    can_invite_users=True,
+    can_pin_messages=False,
+)
+
+@Client.on_message(filters.command(["ban"], cmd) & filters.me)
+
+async def member_ban(client: Client, message: Message):
+    user_id, reason = await extract_user_and_reason(message, sender_chat=True)
+    X = await edit_or_reply(message, "`Currently Fucking Process...`")
+    bot = (await client.get_chat_member(message.chat.id, client.me.id)).privileges
+    if not bot.can_restrict_members:
+        return await X.edit("Ask Admin First dude 💀")
+    if not user_id:
+        return await X.edit("Cannot find User😭.")
+    if user_id == client.me.id:
+        return await X.edit("Examples of stupid fucking coward bitch💀, fuck you!")
+    if user_id in DEVS:
+        return await X.edit("Sorry, That's my developer🌸✨!")
+    if user_id in (await list_admins(client, message.chat.id)):
+        return await X.edit("I can't ban an admin, You know the rules💀, so do i.")
+    try:
+        mention = (await client.get_users(user_id)).mention
+    except IndexError:
+        mention = (
+            message.reply_to_message.sender_chat.title
+            if message.reply_to_message
+            else "Anon"
+        )
+    msg = (
+        f"**Victim Banned:** {mention}\n"
+        f"**In Ban By:** {message.from_user.mention if message.from_user else 'Anon'}\n"
+    )
+    if message.command[0][0] == "d":
+        await message.reply_to_message.delete()
+    if reason:
+        msg += f"**Reason:** {reason}"
+    await message.chat.ban_member(user_id)
+    await X.edit(msg)
