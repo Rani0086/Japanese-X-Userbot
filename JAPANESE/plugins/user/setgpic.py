3842e46f596703043a97628f4ad347abf95a63e9
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 12:23:45 +0530

    Create setgpic.py

---
+++ JAPANESE/plugins/user/setgpic.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.errors import ChatAdminRequired
+from pyrogram.types import ChatPermissions, ChatPrivileges, Message
+
+from config import CMD_HANDLER
+from JAPANESE.nxtgenhelper.adminHelpers import DEVS
+from JAPANESE.nxtgenhelper.basic import edit_or_reply
+from .help import *
+from JAPANESE.utils.misc import extract_user, extract_user_and_reason, list_admins
+
+unmute_permissions = ChatPermissions(
+    can_send_messages=True,
+    can_send_media_messages=True,
+    can_send_polls=True,
+    can_change_info=False,
+    can_invite_users=True,
+    can_pin_messages=False,
+)
