8837768777f88a23d647901aebdff31e73c30f66
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 15:51:15 +0530

    Update kick.py

--- JAPANESE/plugins/user/kick.py
+++ JAPANESE/plugins/user/kick.py
@@ -21,3 +21,33 @@ unmute_permissions = ChatPermissions(
 )
 
 @Client.on_message(filters.command(["kick"], cmd) & filters.me)
+
+async def kick_user(client: Client, message: Message):
+    user_id, reason = await extract_user_and_reason(message)
+    X = await edit_or_reply(message, "`Processing...`")
+    bot = (await client.get_chat_member(message.chat.id, client.me.id)).privileges
+    if not bot.can_restrict_members:
+        return await X.edit("I don't have enough permissions")
+    if not user_id:
+        return await X.edit("I can't find that user.")
+    if user_id == client.me.id:
+        return await X.edit("I can't kick myself.")
+    if user_id == DEVS:
+        return await X.edit("I can't kick my developer")
+    if user_id in (await list_admins(client, message.chat.id)):
+        return await X.edit("I can't kick an admin, You know the rules, so do i.")
+    mention = (await client.get_users(user_id)).mention
+    msg = f"""
+**Kicked User:** {mention}
+**Kicked By:** {message.from_user.mention if message.from_user else 'Anon'}"""
+    if message.command[0][0] == "d":
+        await message.reply_to_message.delete()
+    if reason:
+        msg += f"\n**Reason:** `{reason}`"
+    try:
+        await message.chat.ban_member(user_id)
+        await X.edit(msg)
+        await asyncio.sleep(1)
+        await message.chat.unban_member(user_id)
+    except ChatAdminRequired:
+        return await X.edit("**Sorry You are not an admin**")
