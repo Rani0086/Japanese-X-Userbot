1a651d95f417fdd3d460316287c1a14b903d10f1
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-07-25 15:25:16 +0530

    Update muteandunmute.py

--- JAPANESE/plugins/user/muteandunmute.py
+++ JAPANESE/plugins/user/muteandunmute.py
@@ -21,3 +21,27 @@ unmute_permissions = ChatPermissions(
 )
 
 @Client.on_message(filters.command(["mute", "unmute"], cmd) & filters.me)
+
+async def mute(client: Client, message: Message):
+    user_id, reason = await extract_user_and_reason(message)
+    X = await edit_or_reply(message, "`Processing...`")
+    bot = (await client.get_chat_member(message.chat.id, client.me.id)).privileges
+    if not bot.can_restrict_members:
+        return await X.edit("Ask Admin First")
+    if not user_id:
+        return await X.edit("User not found.")
+    if user_id == client.me.id:
+        return await X.edit("Where Can a Dog!.")
+    if user_id in DEVS:
+        return await X.edit("Can't Get Rid of Stupid Developers!")
+    if user_id in (await list_admins(client, message.chat.id)):
+        return await X.edit("I can't mute an admin, You know the rules, so do i.")
+    mention = (await client.get_users(user_id)).mention
+    msg = (
+        f"**Muted User:** {mention}\n"
+        f"**Muted By:** {message.from_user.mention if message.from_user else 'Anon'}\n"
+    )
+    if reason:
+        msg += f"**Reason:** {reason}"
+    await message.chat.restrict_member(user_id, permissions=ChatPermissions())
+    await X.edit(msg)
